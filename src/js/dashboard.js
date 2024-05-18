bf9e24554ba9272d3e6b78188575e7f67eddcda2
r6732 <rudi.gola@iu-study.org>
2024-05-18 14:39:37 +0200

    Dashboard-Änderungen

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,23 +1,34 @@
 import React, { useState } from 'react';
 import Logo from '../Logo.png';
+import Calendar from '../calendar.png';
+import Checklist from '../checklist.png';
+import Ranking from '../medal.png';
+import shoppingcart from '../shopping-cart.png';
+import Home from '../home.png';
+import Settings from '../settings.png';
+import User from '../user.png';
+import Logout from '../log-out.png';
 import '../css/dashboard.css';
 
 const Dashboard = () => {
     return (
-      <div>
-        <header>
-          <img src={Logo} alt="Logo" />
-          <h1>Alles im grünen Bereich,</h1>
-          {/* Hier könnte beispielsweise ein Menü platziert werden */}
+      <div class="hintergrund">
+         <header>
+        <img src={Logo} alt="Logo" class="logo-image"/>
+        <div class="grauerKreis">
+        <img src={User} alt="User" class="user-image"/>
+        </div>
         </header>
+          {/* Hier könnte beispielsweise ein Menü platziert werden */}
+        
   
         <main>
-        <   p>Dies ist der Hauptinhalt meiner Webseite.</p>
+        <   p>x</p>
         </main>
   
         <footer>
-          <p>&copy; 2024 Meine Webseite. Alle Rechte vorbehalten.</p>
-          {/* Hier könnte der Footer der Seite platziert werden */}
+        <h1 class="slogan">"plan today, change tomorrow!"</h1>
+        <img src={Logout} alt="Logout" class="logout-image"/>
         </footer>
       </div>
     );
