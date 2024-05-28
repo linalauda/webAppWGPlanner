0a93eeca7672806044673bf8ded5c9ab33348a30
linalauda <124688207+linalauda@users.noreply.github.com>
2024-05-28 22:11:29 +0200

    Löse Merge-Konflikt in src/index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Calendar from './js/Calendar';
 import LogIn from './js/login';
 import ToDo from './js/ToDo';
 import Liste from './js/liste';
+import FetchData from './FetchData'; // Neue Komponente importieren
 import './css/index.css';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
         <Route path="/calendar" element={<Calendar />} />
         <Route path="/todo" element={<ToDo />} />
         <Route path="/liste" element={<Liste />} />
+        <Route path="/fetch-data" element={<FetchData />} /> {/* Neue Route hinzufügen */}
       </Routes>
     </BrowserRouter>
   );
