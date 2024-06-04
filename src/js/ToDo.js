f8a4666a5130068029404c0d23204d25e093e1b2
r6732 <rudi.gola@iu-study.org>
2024-06-04 22:44:37 +0200

    Änderungen-Einkaufsliste, ToDos

--- src/js/ToDo.js
+++ src/js/ToDo.js
@@ -18,45 +18,50 @@ function TaskList() {
   };
 
   return (
-    <div class="hintergrund">
+    <div className="hintergrund">
        <Link to="/dashboard">
         <img src={Logo} alt="Logo" className="logo-image" />
         </Link>
-      <div class="outer-flex-container">
-      <h2 className="h3">Deine grüne Bucket-List {currentUser}</h2>
-        <div class="flexcontainer-todos">
-      {userTasks.length > 0 ? (
-        <ul>
-          {userTasks.map((taskWrapper, index) => (
-            <li key={index} style={{ border: `5px solid ${taskWrapper.task.color}`, margin: '30px', padding: '10px' }}>
-              <h3>{taskWrapper.task.title}</h3>
-              <p>{taskWrapper.task.description}</p>
-              <p>Points: {taskWrapper.task.points}</p>
-              <p>Date: {taskWrapper.date}</p>
-              <p>Tip: {taskWrapper.task.tip}</p>
-              <label>
-                <input 
-                  type="checkbox" 
-                  onChange={() => handleCompleteTask(taskWrapper.task.id)} 
-                />
-                Complete
-              </label>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No tasks found for {currentUser}</p>
-      )}
-      <footer>
-          <h1 className="slogan">"plan today, change tomorrow!"</h1>
-          <Link to="/logout">
-            <img src={Logout} alt="Logout" className="logout-image"/>
-          </Link>
-      </footer>
-      </div>
+        <div className="flex-container-user2">
+          <div className="flexbox-grauerKreis2">
+            <img src={User} alt="User" className="flex-item-user-image" />
+          </div>
+          </div>
+      <div className="outer-flex-container">
+        <h2 className="h2">Deine grüne Bucket-List {currentUser}</h2>
+        <div className="scrollable-container">
+          {userTasks.length > 0 ? (
+            <ul className="task-list">
+              {userTasks.map((taskWrapper, index) => (
+                <li key={index} style={{ border: `5px solid ${taskWrapper.task.color}`, margin: '40px', padding: '20px', borderRadius:'25px'  }}>
+                  <h3>{taskWrapper.task.title}</h3>
+                  <p>{taskWrapper.task.description}</p>
+                  <p style={{ fontWeight: 'bold' }}>Punkte: {taskWrapper.task.points}</p>
+                  <p style={{ fontWeight: 'bold' }}>Datum: {taskWrapper.date}</p>
+                  <p style={{ fontWeight: 'bold' }}>Dein Ratschlag: {taskWrapper.task.tip}</p>
+                  <label>
+                    <input 
+                      type="checkbox" 
+                      onChange={() => handleCompleteTask(taskWrapper.task.id)} 
+                    />
+                    Erledigt
+                  </label>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p class="Text-keineAufgaben">Hier könnten Deine To Do's stehen {currentUser}</p>
+          )}
+          <footer>
+            <h1 className="slogan">"plan today, change tomorrow!"</h1>
+            <Link to="/logout">
+              <img src={Logout} alt="Logout" className="logout-image"/>
+            </Link>
+          </footer>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default TaskList;
