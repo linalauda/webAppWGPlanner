3cb82ea48fb5495e378eefd77a58aff7d93faab1
linalauda <124688207+linalauda@users.noreply.github.com>
2024-06-04 11:46:00 +0200

    endpoint anpassung

--- src/FetchData.js
+++ src/FetchData.js
@@ -9,7 +9,7 @@ const FetchData = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:8000');
+        const response = await axios.get('http://coplan.online:8000');
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data: ', error);
