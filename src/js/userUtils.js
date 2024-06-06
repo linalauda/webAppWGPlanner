1629d0bea663be9e647f53110265d6331a3859f5
tijanab256 <tijana.belau@iu-study.org>
2024-06-07 01:48:07 +0200

    anpassungen userUtils

--- src/js/userUtils.js
+++ src/js/userUtils.js
@@ -3,21 +3,21 @@ import { useState } from 'react';
 // Initial list of users
 const initialUsers = [
     { 
-        email: 'mustermann@beispiel.com',
+        email: 'mustermann@beispiel.bsp',
         username: 'Max Mustermann',
         passwort: 'Mustermann123!',
         geddatum: '2001-05-02',
         bio: 'Hallo ich bin Max Mustermann und 23 Jahre alt'
     },
     { 
-        email: 'fiktiv@beispiel.com',
+        email: 'fiktiv@beispiel.bsp',
         username: 'Fritz Fiktiv',
         passwort: 'Fiktiv123!',
         geddatum: '1999-11-25',
         bio: 'Hallo ich bin Fritz Fiktiv und 24 Jahre alt'
     },
     { 
-        email: 'klartext@beispiel.com',
+        email: 'klartext@beispiel.bsp',
         username: 'Klara Klartext',
         passwort: 'klartext123!',
         geddatum: '2003-06-25',
