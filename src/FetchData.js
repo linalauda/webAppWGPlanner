8218ca568b6015b7cabbdd074a61f62a9d5a4eb0
linalauda <124688207+linalauda@users.noreply.github.com>
2024-06-06 11:05:50 +0200

    reg und fetch api endpoint  anpassung

--- src/FetchData.js
+++ src/FetchData.js
@@ -9,7 +9,7 @@ const FetchData = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8000');
+        const response = await axios.get('http://localhost:8000');
         setData(response.data);
       } catch (error) {
         console.error('Error fetching data: ', error);
